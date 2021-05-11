9bf0dbe0f25378d43367ad966acb73340be0b901
Jesper <jesperfriberg94@hotmail.com>
2021-05-11 15:48:27 +0200

    added own files to project

---
+++ src/main/java/com/example/springdocker/service/CarService.java
@@ -0,0 +1,35 @@
+package com.example.springdocker.service;
+
+import com.example.springdocker.model.Car;
+import com.example.springdocker.model.Food;
+import com.example.springdocker.repository.CarRepository;
+import com.example.springdocker.repository.FoodRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class CarService {
+    private final CarRepository repository;
+
+    public List<Car> getCars() {
+        return repository.findAll();
+    }
+
+    public void saveNewCar(Car car) {
+        repository.save(car);
+    }
+
+    public List<String> getDriveableCars() {
+        // hämtar alla bilar vi kan köra
+        List<Car> drivableCars = repository.findCarByCanIDriveIt(true);
+
+        // returnerar endast Food namnen i en lista
+        return drivableCars.stream()
+                .map(car -> car.getName())
+                .collect(Collectors.toList());
+    }
+}
