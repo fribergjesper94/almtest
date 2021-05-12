beee9d953faa095e47b5410bddc7dc84da82b7ce
Jesper <jesperfriberg94@hotmail.com>
2021-05-12 14:39:45 +0200

    added mymathcalc

---
+++ src/main/java/com/example/springdocker/model/MyMathCalc.java
@@ -0,0 +1,24 @@
+package com.example.springdocker.model;
+
+/**
+ * Created by Jesper Friberg Spång
+ * Date: 2021-05-12
+ * Time: 14:35
+ * Project: almtest
+ * Copyright: MIT
+ */
+public class MyMathCalc {
+
+    public int add (int a, int b) {
+        return a + b;
+    }
+
+    public int multiply (int a, int b) {
+        return a*b;
+    }
+
+    public int divide (int a, int b) {
+        return a/b;
+    }
+
+}
