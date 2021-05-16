cc690add4dde3d7e167a01af73c2d5c1fb46b92b
Jesper <jesperfriberg94@hotmail.com>
2021-05-16 18:59:55 +0200

    added tests to all my calc methods

--- src/main/java/com/example/springdocker/model/MyMathCalc.java
+++ src/main/java/com/example/springdocker/model/MyMathCalc.java
@@ -18,7 +18,7 @@ public class MyMathCalc {
     }
 
 
-    public double divide (double a, double b) {
+    public double divide (int a, int b) {
         return a/b;
 }
 }
---
+++ src/test/java/com/example/springdocker/model/MyMathCalcTest.java
@@ -0,0 +1,47 @@
+package com.example.springdocker.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Jesper Friberg Spång
+ * Date: 2021-05-14
+ * Time: 14:25
+ * Project: almtest
+ * Copyright: MIT
+ */
+class MyMathCalcTest {
+
+
+    @Test
+    public void addTest () {
+        MyMathCalc myMathCalc = new MyMathCalc();
+        assertEquals(4,myMathCalc.add(2,2));
+        assertNotEquals(3, myMathCalc.add(5,5));
+    }
+
+    @Test
+    public void multiplyTest () {
+        MyMathCalc myMathCalc = new MyMathCalc();
+        assertEquals(10, myMathCalc.multiply(5,2));
+        assertNotEquals(20, myMathCalc.multiply(3,7));
+    }
+
+    @Test
+    public void divideTest () {
+        MyMathCalc myMathCalc = new MyMathCalc();
+        assertEquals(2, myMathCalc.divide(10,5));
+        assertNotEquals(1, myMathCalc.divide(100,10));
+    }
+
+    @Test
+    @DisplayName("Throws an exception when trying to divide by zero")
+    // Java doesn't throw an exception if the divide method returns a double, it does if it returns an integer.
+    public void divideThrowsTest () {
+        MyMathCalc myMathCalc = new MyMathCalc();
+        assertThrows(ArithmeticException.class, () -> myMathCalc.divide(1,0));
+    }
+
+}
